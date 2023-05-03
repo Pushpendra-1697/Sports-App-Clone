add3439ffefdc47c9a2d9ffc8e5cc75171e528f6
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-05-03 14:22:08 +0530

    added frontend and basic folder structure with dependencies

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <ChakraProvider toastOptions={{ defaultOptions: { position: 'top' } }}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </Provider>
+
+);
